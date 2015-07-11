8c6d2fb610a527ea92efb59f2e20836bb0a0d06e
Piet Bos <info@senlinonline.com>
2015-07-11 23:18:44 +0800

    adjust version

--- so-customizer-hide-menu.php
+++ so-customizer-hide-menu.php
@@ -47,7 +47,7 @@ class SOCHM_Load {
 	function constants() {
 
 		/* Set the version number of the plugin. */
-		define( 'SOCHM_VERSION', '1.0' );
+		define( 'SOCHM_VERSION', '1.0.1' );
 
 		/* Set constant path to the plugin URL. */
 		define( 'SOCHM_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
